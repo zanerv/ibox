227c0b5dc57a173c77d60306d92686fc6e5e98e8
Z <zanerv@gmail.com>
2020-03-29 15:27:57 +0200

    Update ddns.sh

--- opt/ddns.sh
+++ opt/ddns.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 dns=8.8.8.8
 check() {
   ping ${dns} -c 1 -i .2 >/dev/null 2>&1
@@ -7,28 +5,26 @@ check() {
 }
 check
 
-IP() {
-  i=0
-  while [ ${i} -lt 10 ]; do
-    IP=$(host $(hostname -f) ${dns} | awk '{print $4}' | tr -d '[:space:]')
-    NewIP=$(curl -s https://ifconfig.me| tr -d '[:space:]')
-    if [[ $? -gt 0 ]]; then
-      break
-    fi
-    if [[ ${IP} =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ && -n ${NewIP} ]]; then
-      break
-    fi
-    sleep 5
-    i=$(( ${i} + 1 ))
-  done
-}
-
-if [[ ${ONLINE} -eq 1 ]]; then
-  sleep 1m
-  check
-else
-  IP
-  if [[ ${IP} != ${NewIP} && -n ${NewIP} && -n ${IP} ]]; then
-   curl -su ${1}:${2} "https://${3}/dyndns.php?hostname=$(hostname)&myip=${NewIP}"
+i=0
+while [ ${i} -lt 10 ]; do
+  if [[ ${ONLINE} -eq 1 ]]; then
+     sleep 1m
+     check
+     continue
   fi
+  IP=$(host $(hostname -f) ${dns} | awk '{print $4}' | tr -d '[:space:]')
+  NewIP=$(curl -s https://ifconfig.me| tr -d '[:space:]')
+  if [[ $? -gt 0 ]]; then
+    break
+  fi
+  if [[ ${IP} =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ && -n ${NewIP} ]]; then
+    break
+  fi
+  sleep 5
+  i=$(( ${i} + 1 ))
+done
+if [[ ${IP} != ${NewIP} && -n ${NewIP} && -n ${IP} ]]; then
+   curl -su ${1}:${2} "https://${3}/ibox.php?hostname=$(hostname)&myip=${NewIP}"
 fi
+
+
